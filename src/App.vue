f05c73c71fc655a65b5ecd2d0ab2690cb2378f73
Shikataramuno <nomura.takashi@maia.eonet.ne.jp>
2019-01-23 22:37:45 +0900

    pngアイコンの実装例

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <SampleIconView/>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import SampleIconView from './components/SampleIconView.vue';
+
+@Component({
+  components: {
+    SampleIconView,
+  },
+})
+export default class App extends Vue {}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/SampleIconView.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="container-filed">
+    <h1>Vue + Typescript で 自前のアイコンを操作してみる</h1>
+    <div class="sample-icon-field">
+      <img src="../assets/SampleIcon.png" class="icon" width='30px' height='30px' @click="iconClicked"/>
+      <button @click="iconClicked">ダイアログの表示</button>
+      <p>{{message}}</p>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue } from 'vue-property-decorator';
+
+export default class SampleIconView extends Vue {
+  name: string = 'SampleIconView';
+  message: string = '';
+  clickCount: number = 0;
+  iconClicked(): void {
+    alert('アイコンがクリックされました！');
+    this.message = 'アイコンが X回 クリックされました！';
+    this.clickCount++;
+  }
+  initialize(): void {
+    this.clickCount = 0;
+    this.message = '初期化されました';
+  }
+  created(): void {
+    console.log('SampleIconView.created');
+    this.initialize();
+  }
+}
+</script>
+
+<style scoped>
+.sample-icon-field {
+  padding: 5px;
+  width: 100%;
+}
+</style>
